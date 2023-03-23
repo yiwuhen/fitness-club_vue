cc25fd39ad5d8ed53d912c0327b712c29ad44645
lalalahao <2639448497@qq.com>
2023-03-23 21:06:24 +0800

    前端页面

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  color: #2c3e50;
+}
+</style>
